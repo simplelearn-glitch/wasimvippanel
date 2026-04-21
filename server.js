8af0efe9e5a2759216441b294bf2472bb746c477
simplelearn-glitch <wasimkhanpremium4@gmail.com>
2026-04-21 16:58:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -71,4 +71,8 @@ app.post('/generate', async (req, res) => {
 
 // ===== START SERVER =====
 const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("API is running 🚀");
+});
 app.listen(PORT, () => console.log("Server running on port " + PORT));
